69fef420acf569ad1213f998be78a92864754d82
Sina Farhang Doust <sina.farhangdoust@gmail.com>
2025-11-23 09:29:26 +0100

    feat: moved the utility funcitons into utils.py file

--- linkedin_wrapper.py
+++ linkedin_wrapper.py
@@ -1,6 +1,6 @@
 import asyncio
 import json
-import random
+
 import logging
 from typing import List, Tuple, Literal, Callable
 import re
@@ -16,6 +16,10 @@ from constants import (
     DETAIL_LOCATION_PATTERN,
     DETAIL_DESCRIPTION_PATTERN,
 )
+from utils import (
+    ahttp_with_retry,
+    async_with_concurrency
+)
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
@@ -27,65 +31,6 @@ class Job(BaseModel):
     description: str = None
     company: str = None
     location: str = None
-
-
-async def ahttp_with_retry(
-          client: httpx.AsyncClient,
-          url: str,
-          params: dict = None,
-          headers: dict = None,
-          retries: int = 3,
-          random_wait: bool = True,
-) -> str | None:
-    """
-    Makes an HTTP GET request with random delays, headers, and retry logic.
-    """
-    for attempt in range(retries):
-        try:
-            # Random Delay (Human-like behavior)
-            # Sleep between 2 and 5 seconds to respect rate limits
-            if random_wait:
-                delay = random.uniform(2, 5)
-                logger.info(f"Sleeping for {delay:.2f}s before requesting...")
-                await asyncio.sleep(delay)
-
-            logger.info(f"Fetching URL: {url} (Attempt {attempt + 1}/{retries})")
-            response = await client.get(url, params=params, headers=headers, timeout=15.0)
-
-            if response.status_code == 429:
-                logger.warning("Rate limit hit (429). Cooling down for 10 seconds...")
-                await asyncio.sleep(10)
-                continue
-
-            response.raise_for_status()
-
-            return response.text
-
-        except httpx.HTTPStatusError as e:
-            logger.error(f"HTTP error: {e.response.status_code} - {e}")
-            if e.response.status_code == 404:
-                return None
-        except httpx.RequestError as e:
-            logger.error(f"Connection error: {e}")
-
-    logger.error(f"Failed to fetch {url} after {retries} attempts.")
-    return None
-
-async def async_with_concurrency(
-          func: Callable,
-          semaphore: asyncio.Semaphore,
-          **kwargs
-):
-    """
-    Execute an asynchronous function while respecting a concurrency limit.
-
-    :param func: An awaitable function to be executed.
-    :param semaphore: The semaphore controlling how many tasks may run at once.
-    :param kwargs: Arbitrary keyword arguments passed directly to `func`
-    :return:
-    """
-    async with semaphore:
-        return await func(**kwargs)
 
 
 class LinkedinWrapper:
@@ -261,6 +206,7 @@ class LinkedinWrapper:
                 client=self.ahttp_client,
                 url=self.base_search_url,
                 params=params,
+                random_wait=False
             )
             if response:
                 processed_response = self.process_jobs(response)
@@ -287,6 +233,7 @@ class LinkedinWrapper:
         return await asyncio.gather(*job_info_futures, return_exceptions=True)
 
 
+# Usage example
 async def main():
         keywords = "Machine Learning Engineer"
         location = "Denmark"
@@ -296,7 +243,6 @@ async def main():
             location=location,
             n_jobs=50,
         )
-        print()
 
 if __name__ == "__main__":
     asyncio.run(main())
